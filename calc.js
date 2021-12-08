9254f81be61674e70b29183482407be0df98a81c
basil4ever <basil4ever@mail.ru>
2021-12-08 11:09:17 +0200

    %

--- calc.js
+++ calc.js
@@ -4,7 +4,7 @@ let sign = '';
 let finish = false;
 
 const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-const action = ['-', '+', '*', '/', '%', '+/-'];
+const action = ['-', '+', '*', '/', '%'];
 
 const out = document.querySelector('.calc-screen p');
 
